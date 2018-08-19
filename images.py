a21f3bfb28272ec09c05e1b1d4b7bd445ac77acd
Cheryl Siy <cherylsiy@Cheryls-MacBook.local>
2018-08-19 20:29:43 +0800

    fixed greyscale image

--- images.py
+++ images.py
@@ -1,13 +1,19 @@
 import numpy as np
+from scipy import misc
 from scipy.misc import imread, imresize
 import matplotlib.pyplot as plt
 
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+
 img = imread('pikachu.jpeg')
-img_tinted = img*[0.5,0.95,0.9]
+greyscale = rgb2gray(img)
 
 plt.subplot(1,2,1)
 plt.imshow(img)
 
 plt.subplot(1,2,2)
-plt.imshow(np.uint8(img_tinted))
+plt.imshow(np.uint8(greyscale),cmap = plt.get_cmap('gray'))
 plt.show()
+
